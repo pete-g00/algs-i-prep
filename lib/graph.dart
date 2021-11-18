0d5c5bacf78b51cb595958ad7620810939931992
Pete Gautam <2481471g@student.gla.ac.uk>
2021-11-18 19:43:41 +0000

    Added log messages to graph algorithms

--- lib/graph.dart
+++ lib/graph.dart
@@ -41,7 +41,7 @@ class Graph {
     // visit the vertices this vertex is incident to and has not yet been visited
     for (final vertexIndex in vertex.adjList) {
       if (!vertices[vertexIndex]._visited) {
-        // print(vertexIndex);
+        print('Visiting the vertex ${vertices[vertexIndex]} through the vertex $vertex');
         _dfsVisit(vertices[vertexIndex]);
       }
     }
@@ -54,12 +54,13 @@ class Graph {
       vertex._visited = false;
     }
 
+    print('Visiting all the vertices in the graph using Depth-First Search');
     // visit vertices until they are all visited
     for (final vertex in vertices) {
       if (!vertex._visited) {
-        // print(vertex.index);
+        print('Starting this iteration from the vertex $vertex');
         _dfsVisit(vertex);
-        // print('');
+        print('-'*100);
       }
     }
   }
@@ -67,20 +68,24 @@ class Graph {
   void _bfsVisit(Queue<Vertex> queue) {
     // until the queue is empty,
     while (queue.isNotEmpty) {
-      // remove the first element from the queue and visit it
+      // remove the first element from the queue
       final vertex = queue.removeFirst();
-      // this vertex has been visited
-      vertex._visited = true;
+      // visit the vertex if it hasn't been visited
+      if (!vertex._visited) {
+        // this vertex has been visited
+        vertex._visited = true;
+        print('Visiting the vertex $vertex');
 
-      // visit the vertices this vertex is incident to and has not yet been visited
-      for (final vertexIndex in vertex.adjList) {
-        if (!vertices[vertexIndex]._visited) {
-          // add it to the end of the queue to be visited
-          // print(vertexIndex);
-          queue.addLast(vertices[vertexIndex]);
+        // visit the vertices this vertex is incident to and has not yet been visited
+        for (final vertexIndex in vertex.adjList) {
+          if (!vertices[vertexIndex]._visited) {
+            // add it to the end of the queue to be visited
+            print('Added the ${vertices[vertexIndex]} to the queue');
+            queue.addLast(vertices[vertexIndex]);
+          }
         }
+        print('-'*100);
       }
-      // print(queue);
     }
   }
 
@@ -91,15 +96,15 @@ class Graph {
       vertex._visited = false;
     }
 
+    print('Visiting all the vertices in the graph using Depth-First Search');
     // visit vertices until they are all visited
     final queue = Queue<Vertex>();
     for (final vertex in vertices) {
       // if the vertex hasn't been visited, add it to the end of the queue and visit the vertex
       if (!vertex._visited) {
         queue.addLast(vertex);
-        // print(vertex.index);
+        print('Starting this iteration the vertex $vertex');
         _bfsVisit(queue);
-        // print('');
       }
     }
   }
@@ -115,13 +120,15 @@ class Graph {
     }
 
     // the in-degree of a vertex with respect to the vertices still unlabelled
-    final count = <int, int>{};
+    final count = List.filled(vertices.length, 0);
     // the queue containing source vertices with respect to the vertices still unlabelled
     final sourceQueue = Queue<int>();
     // the order of the vertices
     final order = <int>[];
     // the adjacency list of a vertex
     List<int> adjList;
+
+    print('Topologically sorting the graph.');
     
     // initialise the count- compute the in-degree of a vertex
     for (var i=0; i<vertices.length; i++) {
@@ -130,31 +137,38 @@ class Graph {
         count[adjList[j]] = (count[adjList[j]] ?? 0) + 1;
       }
     }
-    
+    print('Initialised the count for all indices:');
+    print('\t$count');
+
     // add all the source vertices to the queue
     for (var i=0; i<vertices.length; i++) {
-      if (!count.containsKey(i)) {
+      if (count[i] == 0) {
+        print('The vertex v$i is a source vertex.');
         sourceQueue.addLast(i);
       }
     }
-
     // the index of the removed source vertex
     int i;
     // until the source queue is empty,
     while (sourceQueue.isNotEmpty) {
+      print('-'*100);
       // remove a source vertex and label it
       i = sourceQueue.removeFirst();
+      print('Labelling vertex v$i with value ${order.length}');
       order.add(i);
       
       // decrement the in-degree of all the vertices that are incident to this vertex
       for (var j=0; j<vertices[i].adjList.length; j++) {
         count[vertices[i].adjList[j]] --;
-
+        
         // if any of them become source vertices, add them to the queue
         if (count[vertices[i].adjList[j]] == 0) {
+          print('The vertex v${vertices[i].adjList[j]} is now a source vertex.');
           sourceQueue.addLast(vertices[i].adjList[j]);
         }
       }
+      print('The count for the indices is:');
+      print('\t$count');
     }
 
     // the list order has all the vertices if and only if it is a DAG
@@ -210,5 +224,5 @@ Graph _createGraph() {
 
 void main(List<String> args) {
   final graph = _createGraph();
-  print(graph.topSort());
+  graph.topSort();
 }
